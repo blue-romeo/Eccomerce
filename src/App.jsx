d0c335db372a932b10aafaa04109bfe9eba38f27
blueromeo <romeoangasa@gmail.com>
2025-05-21 13:32:43 +0300

    Structuring the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Recommended from './Recommended/Recommended'
+import Navigation from './Navigation/Navigation'
+import Product from './Product/Product'
 
 const App = () => {
   return (
     <div>
-      app
+      <Recommended />
+      <Navigation />
+      <Product />
     </div>
   )
 }
